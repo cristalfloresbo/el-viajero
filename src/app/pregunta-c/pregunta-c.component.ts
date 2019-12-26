75639fd43620bc2bc2b5ec195b901110ded9a5e6
Fernando Soto <39037895+K4waiiChan@users.noreply.github.com>
2019-12-26 14:26:53 -0400

    bar graph added to question c

--- src/app/pregunta-c/pregunta-c.component.ts
+++ src/app/pregunta-c/pregunta-c.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ChartOptions, ChartType } from 'chart.js';
+import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
 import { Label, SingleDataSet } from 'ng2-charts';
 import { DataService } from '../services/data.service';
 import { AnswerB, Package } from '../models/answer-b.model';
@@ -31,6 +31,40 @@ export class PreguntaCComponent implements OnInit {
   pieChartPlugins = [];
   years = [{year: '2017', value: 0}, {year: '2018', value: 1}, {year: '2019', value: 2}];
 
+  barChartOptions: ChartOptions = {
+    maintainAspectRatio: false,
+    responsive: true,
+    legend: {
+      labels: {
+        fontColor: 'white'
+      }
+    },
+    scales: {
+      xAxes: [
+        {
+          gridLines: { color: 'rgba(255,255,255,0.1)' },
+          ticks: {
+            fontColor: 'white',
+          }
+        }
+      ],
+      yAxes: [
+        {
+          gridLines: { color: 'rgba(255,255,255,0.1)' },
+          ticks: {
+            fontColor: 'white',
+            beginAtZero: true
+          },
+        }
+      ]
+    }
+  };
+  barChartLabels: Label[];
+  barChartType: ChartType = 'bar';
+  barChartLegend = true;
+  barChartPlugins = [];
+  barChartData: ChartDataSets[];
+
   constructor(public dataService: DataService, public formBuilder: FormBuilder) {
     this.formData = this.formBuilder.group({
       city: new FormControl(),
@@ -42,7 +76,7 @@ export class PreguntaCComponent implements OnInit {
     this.dataService.getQuestionCData().subscribe((response: AnswerC[]) => {
       this.answers = response;
       this.formData.get('city').setValue(response[0]);
-      this.updateGrapichData();
+      this.updateBarGrapichData();
     });
   }
 
@@ -59,8 +93,30 @@ export class PreguntaCComponent implements OnInit {
     });
   }
 
+  updateBarGrapichData() {
+    const answer = this.formData.get('city').value;
+    this.barChartData = [{ data: [], label: '2017'}, { data: [], label: '2018'}, { data: [], label: '2019' }];
+    this.barChartLabels = [];
+    answer.hotels.forEach((hotel: Hotel) => {
+      this.barChartLabels.push(hotel.name);
+      this.barChartData[0].data.push(hotel.incomePerYear[0].income);
+      this.barChartData[1].data.push(hotel.incomePerYear[1].income);
+      this.barChartData[2].data.push(hotel.incomePerYear[2].income);
+    });
+    this.updateGrapichData();
+  }
+
   getCityAndTotalIncome()  {
-    return `En la ciudad de ${this.formData.get('city').value.city}
-            se entre todos los hoteles el total de ganancias fue de: ${this.totalIncome}`;
+    const yearIndex = this.formData.get('year').value;
+    let year: number;
+    if (yearIndex === 0) {
+      year = 2017;
+    } else if ( yearIndex === 1) {
+      year = 2018;
+    } else {
+      year = 2019;
+    }
+    return `El año ${year} entre todos los hoteles en la ciudad de ${this.formData.get('city').value.city}
+            el total de ganancias fue de: ${this.totalIncome}`;
   }
 }
